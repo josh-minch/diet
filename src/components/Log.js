ff551c476904e312bf5106815a43aea13e0362d3
Josh Minch <josh.minch@gmail.com>
2022-01-21 19:22:53 -0800

    Add food group log req displays

--- src/components/Log.js
+++ src/components/Log.js
@@ -3,11 +3,32 @@ import {
     Box,
     Heading,
     Spacer,
-    VStack,
     Stack,
-    Flex,
+    Text
 } from '@chakra-ui/react'
 import { FoodItem } from './FoodItem'
+import { FoodGroup } from './FoodGroup'
+import { req } from '../req/req'
+
+
+
+const Req = ({ myFoodStateFoodGroup, foodGroupReq }) => {
+    let totalFoodGroupQuantity = 0
+    if (myFoodStateFoodGroup.length > 0) {
+        totalFoodGroupQuantity = myFoodStateFoodGroup
+            .map(food => food.servings())
+            .reduce((prev, cur) => prev + cur, 0)
+    }
+
+    return (
+        <>
+            <Heading size='sm'>{foodGroupReq.group}</Heading>
+            <Text>{totalFoodGroupQuantity} / {foodGroupReq.req}</Text>
+        </>
+    )
+};
+
+
 export const Log = ({ myFoodState, setMyFoodState }) => {
     return (
         <>
@@ -24,14 +45,19 @@ export const Log = ({ myFoodState, setMyFoodState }) => {
                     <Heading mb={2} size={'md'}>
                         Summary
                     </Heading>
-                    {myFoodState
+                    {req.map(r => {
+                        const myFoodStateFoodGroup = myFoodState.filter(food => food.group === r.group)
+                        return <Req key={r.group} myFoodStateFoodGroup={myFoodStateFoodGroup} foodGroupReq={r} />
+                    })}
+
+                    {/* {myFoodState
                         .filter(food => food.group === 'green')
                         .map(food => food.quantity)
                         .reduce(
                             (prev, cur) => prev + cur,
                             0
                         )
-                    }
+                    } */}
                 </Box>
             </Stack>
         </>
